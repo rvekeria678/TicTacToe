cbf5f8b56a49ce1f6ee3ced3babe6b3b4e0ed9e5
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-03-29 16:11:05 -0400

    Added Tile Images

--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,18 @@ let control = false, p1_color = 'bg-red-300', p2_color = 'bg-green-300';
 tiles.forEach((el)=>{
     el.addEventListener("click", (event)=> {
         value = Number(event.target.getAttribute('name'));
-        if (board[value] == undefined) {
+        if (board[value] === undefined) {
             board[value] = Number(control);
-            el.classList.add(control ? p1_color : p2_color);
+            el.style.backgroundImage = control ? "url('../assets/images/o.png')" : "url('../assets/images/o.png')"
+            control = control ? false : true;
         }
         console.log(board);
-        control = control ? false : true;
     });
 });
-
 function checkBoardState(e_val) {
     switch(e_val) {
         case 0:
+            
             break;
         case 1:
             break;
@@ -24,5 +24,21 @@ function checkBoardState(e_val) {
             break;
         case 3:
             break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
     }
 }
+
+const l1 = 0, l2 = 1;
+
+const var1 = l2 || l1;
+
+console.log(var1);
