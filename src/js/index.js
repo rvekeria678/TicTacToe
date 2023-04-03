55e5f0b42670ecc00577ffc69bd45d15588448c6
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-04-03 11:53:52 -0400

    Fixed Reset Bug

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,17 @@ const tiles = document.querySelectorAll('.tile');
 const reset = document.querySelector('#reset');
 const game_msg = document.querySelector("#game-msg");
 const board = [];
-let control = false, p1_color = 'bg-red-300', p2_color = 'bg-green-300';
+let control = false, playing = true, p1_color = 'bg-red-300', p2_color = 'bg-green-300';
 
 tiles.forEach((el)=>{
     el.addEventListener("click", (event)=> {
         value = Number(event.target.getAttribute('name'));
-        if (board[value] === undefined) {
+        if (board[value] === undefined && playing) {
             board[value] = Number(control);
             el.style.backgroundImage = control ? "url(./src/images/o.png)" : "url(./src/images/x2.webp)";
             el.style.backgroundSize = '100% 100%';
             control = control ? false : true;
         }
-        console.log(board);
         checkBoardState(value);
     });
 });
@@ -24,6 +23,7 @@ reset.addEventListener('click', (event)=> {
     });
     board.length = 0;
     game_msg.innerText = "";
+    playing = true;
 });
 
 function checkBoardState(e_val) {
@@ -33,12 +33,14 @@ function checkBoardState(e_val) {
                 (board[0] === board[3] && board[0] === board[6]) ||
                 (board[0] === board[4] && board[0] === board[8])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 1:
             if ((board[0] === board[1] && board[0] === board[2]) ||
                 (board[1] === board[4] && board[1] === board[7])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 2:
@@ -46,12 +48,14 @@ function checkBoardState(e_val) {
                 (board[2] === board[5] && board[2] === board[8]) ||
                 (board[2] === board[4] && board[2] === board[7])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 3:
             if ((board[0] === board[3] && board[0] === board[6]) ||
                 (board[3] === board[4] && board[3] === board[5])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 4:
@@ -59,13 +63,15 @@ function checkBoardState(e_val) {
                 (board[4] === board[1] && board[4] === board[7]) ||
                 (board[4] === board[2] && board[4] === board[6]) ||
                 (board[4] === board[0] && board[4] === board[8])) {
-                console.log(control ? "Player 1 won!" : "Player 2 won!");
+                game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 5:
             if ((board[5] === board[2] && board[5] === board[8]) ||
                 (board[5] === board[3] && board[5] === board[4])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 6:
@@ -73,12 +79,14 @@ function checkBoardState(e_val) {
                 (board[6] === board[4] && board[6] === board[2]) ||
                 (board[6] === board[3] && board[6] === board[0])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 7:
             if ((board[7] === board[6] && board[7] === board[8]) ||
                 (board[7] === board[4] && board[7] === board[1])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
         case 8:
@@ -86,7 +94,9 @@ function checkBoardState(e_val) {
                 (board[8] === board[2] && board[8] === board[5]) ||
                 (board[8] === board[4] && board[8] === board[0])) {
                 game_msg.innerText = control ? "Player 1 Won!" : "Player 2 Won!";
+                playing = false;
             }
             break;
     }
+    console.log(board);
 }
